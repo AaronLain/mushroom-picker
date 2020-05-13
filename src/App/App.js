f46064781617b4947fcf216fd58493c73d979504
Aaron Lain <aaron.j.lain@gmail.com>
2020-05-13 18:12:21 -0500

    cards print to forest

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.scss';
+import mushroomData from '../helpers/data/mushroomData';
+import Forest from '../components/Forest/Forest';
 
 class App extends React.Component {
+  state = {
+    forest: [],
+  }
+
+  componentDidMount() {
+    const forest = mushroomData.getMushrooms();
+    this.setState({ forest });
+  }
+
   render() {
     return (
       <div className="App">
         <h2>HELLO THERE</h2>
         <button className="btn btn-info">LOL</button>
+        <Forest forest={ this.state.forest }/>
       </div>
     );
   }
